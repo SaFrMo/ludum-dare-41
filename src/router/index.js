1a360db2ac47866c19c4e796f64f574151b3f261
Sander Moolin <sandermoolin@gmail.com>
2018-04-20 17:04:57 -0400

    Added boilerplate

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import FrontPage from '@/views/FrontPage'
+
+Vue.use(Router)
+
+export default new Router({
+    routes: [
+        {
+            path: '/',
+            name: 'FrontPage',
+            component: FrontPage
+        }
+    ]
+})
---
+++ src/views/FrontPage.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Ludum Dare 41</h1>
+</template>
